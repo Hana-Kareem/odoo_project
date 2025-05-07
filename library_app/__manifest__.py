c1a0e6257c2a1dc8000b0eccb4b0bdac9cd305db
hana-atef <hanakareem2002@gmail.com>
2025-05-07 06:14:59 +0300

    add publisher

---
+++ library_app/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': "Library App",
+    'summary':""" """,
+    'description': """ """,
+    'auther': "Hana Atef",
+    'category': "Productivity",
+    'version':"17.0.0.1.0",
+    'depends':['base',
+               ],
+    'application': True,
+    'data': [
+        'security/ir.model.access.csv',
+        'views/base_menus.xml',
+        'views/book.xml'
+        'views/publisher.xml',
+    ],
+}
